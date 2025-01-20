88c7f8df3bd1400be3c6c7695382dcee64f9bce2
Fırat Akkemik <mrx@mrx.local>
2025-01-20 15:25:25 +0300

    Docker Projects

---
+++ java-hello-world/App.java
@@ -0,0 +1,25 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello, World!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+        System.out.println("Server started at http://localhost:8080");
+        server.start();
+    }
+}
+
